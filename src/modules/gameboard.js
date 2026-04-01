08ecff7426167a296d1bbe832bc4499af958a6dd
gotw <64510900+G-O-T-W@users.noreply.github.com>
2026-04-01 18:28:46 +0530

    fix: revamp placeShip logic

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,27 +1,88 @@
-import Ships from './ships';
+import Ships from './ships.js';
 
 export default class Gameboard {
   constructor() {
-    this.grid = [];
     this.missedShots = [];
+    this.rows = 10;
+    this.cols = 10;
+    this.grid = Array(this.rows) // instantiate the grid
+      .fill()
+      .map(() => Array(this.cols).fill(-1));
+  }
+
+  findOrientation(posA, posB) {
+    let lenX = posA[0] - posB[0]; // lenX is difference in x co-ordinate
+    if (lenX == 0) return 'h';
+    else return 'v';
+  }
+
+  findLength(orientation, posA, posB) {
+    if (orientation == 'h') return Math.abs(posA[1] - posB[1]) + 1;
+    else return Math.abs(posA[0] - posB[0]) + 1;
+  }
+
+  findRange(orientation, posA, posB) {
+    let start, end;
+    if (orientation == 'h') {
+      // place horizonatlly
+      start = posA[1] < posB[1] ? posA[1] : posB[1];
+      end = start == posA[1] ? posB[1] : posA[1];
+    } else {
+      // place vertically
+      start = posA[0] < posB[0] ? posA[0] : posB[0];
+      end = start == posA[0] ? posB[0] : posA[0];
+    }
+    return [start, end];
+  }
+
+  isEmpty(orientation, posA, posB) {
+    const [start, end] = this.findRange(orientation, posA, posB);
+    if (orientation == 'h') {
+      const i = posA[0];
+      for (let j = start; j <= end; j++) {
+        if (this.grid[i][j] != -1) return false;
+      }
+    } else {
+      const j = posA[1];
+      for (let i = start; i <= end; i++) {
+        if (this.grid[i][j] != -1) return false;
+      }
+    }
+    return true;
+  }
+
+  isValid(posA, posB) {
+    return (
+      posA[0] < this.rows &&
+      posA[0] >= 0 &&
+      posA[1] < this.cols &&
+      posA[1] >= 0 &&
+      posB[0] < this.rows &&
+      posB[0] >= 0 &&
+      posB[1] < this.cols &&
+      posB[1] >= 0
+    );
   }
 
   placeShip(posA, posB) {
-    let length = posA[0] - posB[0];
-    if (length != 0 && posA[1] == posB[1]) {
-      const ship = new Ships(length);
-      for (let i = posA[0]; i <= posB[0]; i++) {
-        if (this.grid[i] === undefined) this.grid[i] = []; 
-        this.grid[i][posA[1]] = ship;
+    const orientation = this.findOrientation(posA, posB);
+    // don't place ship if co-ordinates are filled already or if out of bounds
+    if (!this.isEmpty(orientation, posA, posB) || !this.isValid(posA, posB))
+      return;
+    const length = this.findLength(orientation, posA, posB);
+    const ship = new Ships(length);
+    const [start, end] = this.findRange(orientation, posA, posB);
+    if (orientation == 'h') {
+      const i = posA[0];
+      for (let j = start; j <= end; j++) {
+        this.grid[i][j] = ship;
       }
-    }
-    length = posA[1] - posB[1];
-    if (length != 0 && posA[0] == posB[0]) {
-      const ship = new Ships(length);
-      if (this.grid[posA[0]] === undefined) this.grid[posA[0]] = []; 
-      for (let i = posA[1]; i <= posB[1]; i++) {
-        this.grid[posA[0]][i] = ship;
+    } else {
+      const j = posA[1];
+      for (let i = start; i <= end; i++) {
+        this.grid[i][j] = ship;
       }
     }
   }
 }
+
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -3,10 +3,17 @@ import Gameboard from '../src/modules/gameboard';
 describe('Gameboard class is working correctly', () => {
   const gameBoard = new Gameboard();
   test('placeShip() function is working as intended', () => {
+    gameBoard.placeShip([0, 0], [3, 0]);
+    expect(gameBoard.grid[0][0]).not.toEqual(-1);
+    expect(gameBoard.grid[1][0]).not.toEqual(-1);
+    expect(gameBoard.grid[2][0]).not.toEqual(-1);
+    expect(gameBoard.grid[3][0]).not.toEqual(-1);
+  });
+  test('placeShip() does override existing ship placements', () => {
     gameBoard.placeShip([0, 0], [0, 3]);
-    expect(gameBoard.grid[0][0]).toBeDefined();
-    expect(gameBoard.grid[0][1]).toBeDefined();
-    expect(gameBoard.grid[0][2]).toBeDefined();
+    expect(gameBoard.grid[0][1]).toEqual(-1);
+    expect(gameBoard.grid[0][2]).toEqual(-1);
+    expect(gameBoard.grid[0][3]).toEqual(-1);
   });
   test.todo('receiveAttack() function is working as intended');
 });
